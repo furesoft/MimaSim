079e976b1df9a64a9eed457cecbeb1208f192706
Chris Anders <filmee24@gmail.com>
2020-12-30 19:04:51 +0100

    Add BusRegistry and BusMap

---
+++ Mima-Sim/Core/BusMap.cs
@@ -0,0 +1,27 @@
+﻿using MimaSim.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MimaSim.Core
+{
+
+    public class BusMap : List<string>
+    {
+        public void Activate()
+        {
+            foreach (var id in this)
+            {
+                BusRegistry.ActivateBus(id);
+            }
+        }
+
+        public void Deactivate()
+        {
+            foreach (var id in this)
+            {
+                BusRegistry.DeactivateBus(id);
+            }
+        }
+    }
+}
---
+++ Mima-Sim/Core/BusRegistry.cs
@@ -0,0 +1,55 @@
+﻿using MimaSim.Controls;
+using System.Collections.Generic;
+
+namespace MimaSim.Core
+{
+    public static class BusRegistry
+    {
+        private static Dictionary<string, BusControl> _ids = new Dictionary<string, BusControl>();
+        private static Dictionary<string, BusMap> _maps = new Dictionary<string, BusMap>();
+
+        public static void RegisterBus(string id, BusControl control)
+        {
+            if (!_ids.ContainsKey(id))
+            {
+                _ids.Add(id, control);
+            }
+        }
+
+        public static void RegisterBusMap(string id, BusMap map)
+        {
+            if (!_maps.ContainsKey(id))
+            {
+                _maps.Add(id, map);
+            }
+        }
+
+        public static void ActivateBus(string id)
+        {
+            ChangeBusState(id, BusState.Recieving);
+        }
+
+        private static void ChangeBusState(string id, BusState state)
+        {
+            if (_ids.ContainsKey(id))
+            {
+                _ids[id].State = state;
+            }
+        }
+
+        public static void DeactivateBus(string id)
+        {
+            ChangeBusState(id, BusState.None);
+        }
+
+        public static BusMap GetBusMap(string id)
+        {
+            if (!_maps.ContainsKey(id))
+            {
+                return _maps[id];
+            }
+
+            return null;
+        }
+    }
+}
