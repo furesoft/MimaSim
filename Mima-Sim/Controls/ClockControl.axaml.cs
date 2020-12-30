418d494cd9f8a798f09b3e3feb331c89f0a08558
Chris Anders <filmee24@gmail.com>
2020-12-30 13:52:46 +0100

    Add ClockControl

---
+++ Mima-Sim/Controls/ClockControl.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MimaSim.Controls
+{
+    public class ClockControl : UserControl
+    {
+        public ClockControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
