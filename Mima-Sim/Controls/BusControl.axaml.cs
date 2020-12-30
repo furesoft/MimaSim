018a1a73eb8cf9c05c93cdecb42024869dac58d8
Chris Anders <filmee24@gmail.com>
2020-12-30 13:23:41 +0100

    Added BusControl

---
+++ Mima-Sim/Controls/BusControl.axaml.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace MimaSim.Controls
+{
+    public enum BusState { None, Recieving }
+
+    public class BusControl : UserControl
+    {
+        public BusControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private BusState _state;
+
+        public BusState State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+
+                if (_state == BusState.None)
+                {
+                    Background = Brushes.Gray;
+                }
+                else if (_state == BusState.Recieving)
+                {
+                    Background = Brushes.Red;
+                }
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
