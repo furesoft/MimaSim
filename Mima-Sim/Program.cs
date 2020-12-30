6656ea53339df9dd9b73f23e1a233c6defea84d1
Chris Anders <filmee24@gmail.com>
2020-12-30 17:42:40 +0100

    Tried to implement popups for mima components

--- Mima-Sim/Program.cs
+++ Mima-Sim/Program.cs
@@ -1,11 +1,9 @@
 ﻿using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Logging.Serilog;
-using System;
 
 namespace Mima_Sim
 {
-    class Program
+    internal class Program
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
