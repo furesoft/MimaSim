b22e54ca05e857345c807c4b74b0ef8c415b7d13
Chris Anders <filmee24@gmail.com>
2021-01-10 11:42:22 +0100

    Add Maschinencode Document and Cleanup Projektarbeit_Projektskizze

---
+++ Mima-Sim/Core/PopupSwitch.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MimaSim.Core
+{
+    public static class PopupSwitch
+    {
+        public static void SetPopup(Control control, Control target)
+        {
+        }
+    }
+}
