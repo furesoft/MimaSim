605d61f9ad8c7a1b208ecc83d6391ac14a12409e
Chris Anders <filmee24@gmail.com>
2021-01-10 14:29:45 +0100

    Add Accumulator Registry to MimaControl and Added some Buses to BusMap

---
+++ Mima-Sim/Controls/MimaComponents/Wrappers/AccuWrapper.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MimaSim.Controls.MimaComponents.Wrappers
+{
+    public class AccuWrapper : UserControl
+    {
+        public AccuWrapper()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
