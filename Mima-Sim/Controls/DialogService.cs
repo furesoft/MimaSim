bf05ca8359365c25c9e615cdbc586f611ec29a63
Chris Anders <filmee24@gmail.com>
2021-01-12 16:37:32 +0100

    Add DialogService

---
+++ Mima-Sim/Controls/DialogService.cs
@@ -0,0 +1,37 @@
+﻿namespace MimaSim.Controls
+{
+    public static class DialogService
+    {
+        private static ContentDialog _host;
+
+        public static void SetIsHost(ContentDialog target, bool value)
+        {
+            if (value)
+            {
+                _host = target;
+            }
+        }
+
+        public static bool GetIsHost(ContentDialog target)
+        {
+            return object.ReferenceEquals(_host, target);
+        }
+
+        public static void Open(object content)
+        {
+            if (_host != null)
+            {
+                _host.DialogContent = content;
+                _host.IsOpened = true;
+            }
+        }
+
+        public static void Close()
+        {
+            if (_host != null)
+            {
+                _host.IsOpened = false;
+            }
+        }
+    }
+}
