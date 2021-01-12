cdb4e3a0a835dce456c4721e614ac3676e32f05b
Chris Anders <filmee24@gmail.com>
2021-01-12 16:48:35 +0100

    Add BaseViewModel

---
+++ Mima-Sim/Core/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Markup.Xaml;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MimaSim.Core
+{
+    public abstract class BaseViewModel : MarkupExtension, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Activator.CreateInstance(GetType());
+        }
+
+        protected void Raise([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Mima-Sim/ViewModels/ExecutionTabModel.cs
@@ -0,0 +1,22 @@
+﻿using MimaSim.Controls;
+using MimaSim.Core;
+using System.Windows.Input;
+
+namespace MimaSim.ViewModels
+{
+    public class ExecutionTabModel : BaseViewModel
+    {
+        private ICommand _openCommand;
+
+        public ICommand OpenCommand
+        {
+            get { return _openCommand; }
+            set { _openCommand = value; Raise(); }
+        }
+
+        public ExecutionTabModel()
+        {
+            _openCommand = new DelegateCommand(_ => DialogService.Open());
+        }
+    }
+}
