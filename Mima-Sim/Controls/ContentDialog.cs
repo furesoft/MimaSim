59a939938e23719285765600398d8c17cb6a5567
Chris Anders <filmee24@gmail.com>
2021-01-12 15:40:31 +0100

    Add Base For ContentDialog

---
+++ Mima-Sim/Controls/ContentDialog.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Styling;
+using System;
+
+namespace MimaSim.Controls
+{
+    public class ContentDialog : ContentControl, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(ContentDialog);
+
+        public static AvaloniaProperty<object> DialogContentProperty =
+            AvaloniaProperty.Register<ContentDialog, object>("DialogContent");
+
+        public object DialogContent
+        {
+            get { return GetValue(DialogContentProperty); }
+            set { SetValue(DialogContentProperty, value); }
+        }
+    }
+}
