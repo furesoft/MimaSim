9b69ae76e0d766acbd38f830bd263fc9fc5c5cee
Chris Anders <filmee24@gmail.com>
2021-02-09 16:25:00 +0100

    Add AssemblyParser

---
+++ Mima-Sim/MIMA/Parsing/Parsers/AssemblyParser.cs
@@ -0,0 +1,41 @@
+﻿using MimaSim.Core;
+using MimaSim.Core.AST;
+using MimaSim.Core.Tokenizer;
+using MimaSim.MIMA.VM;
+using System;
+using System.Linq;
+
+namespace MimaSim.MIMA.Parsing.Parsers
+{
+    public class AssemblyParser : IParser
+    {
+        public IAstNode Parse(string input)
+        {
+            var tokenizer = new PrecedenceBasedRegexTokenizer();
+            tokenizer.AddDefinition(TokenKind.AddressLiteral, "&[0-9a-fA-F]{1,6}", 2);
+            tokenizer.AddDefinition(TokenKind.HexLiteral, "0x[0-9a-fA-F]{1,6}", 2);
+            tokenizer.AddDefinition(TokenKind.Register, GetRegisterPattern(), 1);
+
+            var tokens = tokenizer.Tokenize(input);
+            var enumerator = new TokenEnumerator(tokens);
+
+            return ParseInstructionBlock(enumerator);
+        }
+
+        private string GetRegisterPattern()
+        {
+            var names = Enum.GetNames(typeof(Registers));
+            var namesLowered = names.Select(_ => _.ToLower());
+
+            var allNames = names.Concat(namesLowered);
+
+            return string.Join("|", allNames);
+        }
+
+        private IAstNode ParseInstructionBlock(TokenEnumerator enumerator)
+        {
+            //ToDo: implement assembly parser
+            return NodeFactory.Call("{}");
+        }
+    }
+}
---
+++ Mima-Sim/MimaTest/AssemblyParserTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MimaSim.MIMA.Parsing.Parsers;
+
+namespace MimaTest
+{
+    [TestClass]
+    public class AssemblyParserTest
+    {
+        [TestMethod]
+        public void Tokenize_Should_Pass()
+        {
+            var input = "&C0FFEE";
+            var parser = new AssemblyParser();
+
+            var result = parser.Parse(input);
+        }
+    }
+}
