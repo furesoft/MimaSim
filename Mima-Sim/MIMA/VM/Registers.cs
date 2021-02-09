ed857204e545667a09c32bb9a8cb0677bc960a9c
Chris Anders <filmee24@gmail.com>
2021-02-09 16:08:37 +0100

    Add Registers Enumeration

---
+++ Mima-Sim/MIMA/VM/Registers.cs
@@ -0,0 +1,14 @@
+﻿namespace MimaSim.MIMA.VM
+{
+    public enum Registers
+    {
+        Accumulator = 1,
+        X = 2,
+        Y = 3,
+        Z = 4,
+        IR = 5,
+        IAR = 6,
+        SAR = 7,
+        SDR = 8,
+    }
+}
