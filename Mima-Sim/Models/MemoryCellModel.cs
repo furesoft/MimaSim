8d9cf5e072eece4ef9f09982608e80f0881b5802
Chris Anders <filmee24@gmail.com>
2021-02-11 13:47:32 +0100

    Add Content to MemoryPopup

---
+++ Mima-Sim/Models/MemoryCellModel.cs
@@ -0,0 +1,10 @@
+﻿using MimaSim.MIMA;
+
+namespace MimaSim.Models
+{
+    public class MemoryCellModel
+    {
+        public int Address { get; set; }
+        public TinyInt Value { get; set; }
+    }
+}
--- Mima-Sim/ViewModels/MemoryPopupViewModel.cs
+++ Mima-Sim/ViewModels/MemoryPopupViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using MimaSim.Controls;
+using MimaSim.Models;
 using ReactiveUI;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace MimaSim.ViewModels
@@ -9,9 +11,14 @@ namespace MimaSim.ViewModels
         public MemoryPopupViewModel()
         {
             CloseCommand = ReactiveCommand.Create(() => DialogService.Close());
+
+            MemoryCells = new ObservableCollection<MemoryCellModel>();
+            MemoryCells.Add(new MemoryCellModel { Value = 123 });
         }
 
         public ViewModelActivator Activator => new ViewModelActivator();
         public ICommand CloseCommand { get; set; }
+
+        public ObservableCollection<MemoryCellModel> MemoryCells { get; set; }
     }
 }
