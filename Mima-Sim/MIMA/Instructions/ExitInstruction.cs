9893269da85687c9ebb5b1596d756cc087e9e2bb
Chris Anders <filmee24@gmail.com>
2021-02-12 12:03:43 +0100

    Create ExitInstruction.cs

---
+++ Mima-Sim/MIMA/Instructions/ExitInstruction.cs
@@ -0,0 +1,23 @@
+﻿using MimaSim.Core;
+using MimaSim.Messages;
+using MimaSim.MIMA.Components;
+using ReactiveUI;
+
+namespace MimaSim.MIMA.Instructions
+{
+    public class ExitInstruction : IInstruction
+    {
+        public OpCodes Instruction => OpCodes.Exit;
+        public string Mnemonic => "exit";
+
+        public InstructionTypeSizes Size => InstructionTypeSizes.NoArg;
+
+        public bool Invoke(CPU cpu)
+        {
+            cpu.Clock.Stop();
+            MessageBus.Current.SendMessage(new StopMessage());
+
+            return false;
+        }
+    }
+}
