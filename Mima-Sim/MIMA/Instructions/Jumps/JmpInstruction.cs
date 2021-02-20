6d647950e45ca8f14972b94f9997ec4050a51894
Chris Anders <filmee24@gmail.com>
2021-02-20 18:05:02 +0100

    Create JmpInstruction.cs

---
+++ Mima-Sim/MIMA/Instructions/Jumps/JmpInstruction.cs
@@ -0,0 +1,21 @@
+﻿using MimaSim.Core;
+using MimaSim.MIMA.Components;
+
+namespace MimaSim.MIMA.Instructions.Jumps
+{
+    public class JmpInstruction : IInstruction
+    {
+        public OpCodes Instruction => OpCodes.JMP;
+
+        public string Mnemonic => "jmp";
+
+        public bool Invoke(CPU cpu)
+        {
+            var address = cpu.Fetch16();
+
+            cpu.SetRegister(VM.Registers.IAR, address);
+
+            return false;
+        }
+    }
+}
