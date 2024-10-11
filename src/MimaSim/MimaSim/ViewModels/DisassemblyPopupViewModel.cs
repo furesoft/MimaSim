2396ad3484ec4de14169dc3bd2e29e9db0b644bf
Chris Anders <filmee24@gmail.com>
2024-10-11 06:07:39 +0000

    feat: HexDump Toggle

--- src/MimaSim/MimaSim/ViewModels/DisassemblyPopupViewModel.cs
+++ src/MimaSim/MimaSim/ViewModels/DisassemblyPopupViewModel.cs
@@ -10,11 +10,13 @@ namespace MimaSim.ViewModels;
 public class DisassemblyPopupViewModel : ReactiveObject, IActivatableViewModel
 {
     private string _raw;
+    private string _disassembly;
 
     public DisassemblyPopupViewModel()
     {
         CloseCommand = ReactiveCommand.Create(() => DialogService.Close());
         Raw = GetRawString();
+        Disassembly = Disassemble();
     }
 
     public ViewModelActivator Activator => new();
@@ -24,14 +26,36 @@ public class DisassemblyPopupViewModel : ReactiveObject, IActivatableViewModel
     public string Raw
     {
         get => _raw;
-        set { _raw = value; this.RaiseAndSetIfChanged(ref _raw, value); }
+        set { this.RaiseAndSetIfChanged(ref _raw, value); }
     }
 
-    private string GetRawString()
+    public string Disassembly
+    {
+        get => _disassembly;
+        set { this.RaiseAndSetIfChanged(ref _disassembly, value); }
+    }
+
+    private string Disassemble()
     {
         var raw = CPU.Instance.Program;
         var disassembler = new Disassembler(raw);
 
         return disassembler.Disassemble();
+    }
+
+    private string GetRawString()
+    {
+        var raw = CPU.Instance.Program;
+
+        return string.Join(' ', raw.Select(_ =>
+        {
+            var result = (_).ToString("x");
+            if (result.Length == 1)
+            {
+                result = "0" + result;
+            }
+
+            return result;
+        })).ToUpper();
     }
 }
