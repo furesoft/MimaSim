5ae9309c860fd81a0ecc4802120e9e09632fb981
Chris Anders <filmee24@gmail.com>
2024-10-26 20:26:11 +0200

    feat: Add NetworkCard To UI

---
+++ src/MimaSim/MimaSim/Controls/MimaComponents/NetworkCardControl.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MimaSim.Controls.MimaComponents;
+
+public partial class NetworkCardControl : UserControl
+{
+    public NetworkCardControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/MimaSim/MimaSim/Controls/MimaComponents/Wrappers/NetworkCardWrapper.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MimaSim.Controls.MimaComponents.Wrappers;
+
+public partial class NetworkCardWrapper : UserControl
+{
+    public NetworkCardWrapper()
+    {
+        InitializeComponent();
+    }
+}
