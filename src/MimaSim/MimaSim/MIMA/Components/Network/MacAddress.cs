7903b591037d73563ab157552dd0e183ffb3c189
Chris Anders <filmee24@gmail.com>
2024-10-26 21:11:09 +0200

    feat: Add Initial Work for network card

---
+++ src/MimaSim/MimaSim/MIMA/Components/Network/MacAddress.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace MimaSim.MIMA.Components.Network;
+
+public readonly record struct MacAddress(byte[] address)
+{
+    private readonly byte[] _address = address;
+
+    public static MacAddress Parse(string macAddress)
+    {
+        if (!TryParse(macAddress, out var a))
+            throw new FormatException("Ungültiges MAC-Adressformat.");
+
+        return a;
+    }
+
+    public static MacAddress Generate()
+    {
+        var random = new Random();
+        var bytes = new byte[6];
+        random.NextBytes(bytes);
+
+        bytes[0] = (byte)(bytes[0] & 0xFE);
+
+        return new MacAddress(bytes);
+    }
+
+    public static bool TryParse(string macAddress, out MacAddress address)
+    {
+        var cleanedMac = macAddress.Replace(":", "").Replace("-", "").Replace(".", "");
+
+        if (cleanedMac.Length != 12 || !cleanedMac.All(Uri.IsHexDigit))
+        {
+            address = default;
+            return false;
+        }
+
+        address = new MacAddress(Enumerable.Range(0, 6)
+            .Select(i => Convert.ToByte(cleanedMac.Substring(i * 2, 2), 16))
+            .ToArray());
+
+        return true;
+    }
+
+    public override string ToString()
+    {
+        return string.Join(":", _address.Select(b => b.ToString("X2")));
+    }
+
+    public byte[] GetBytes() => _address;
+}
---
+++ src/MimaSim/MimaSim/MIMA/Components/Network/NetworkCard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using MimaSim.Core;
+
+namespace MimaSim.MIMA.Components.Network;
+
+public class NetworkCard
+{
+    public IPAddress IP { get; set; }
+    public MacAddress MAC { get; set; }
+
+    public NetworkCard(ICache cache)
+    {
+        var mac = cache.Get<string>("config.nic.mac");
+        if (mac == null)
+        {
+            MAC = MacAddress.Generate();
+            cache.AddOrUpdate("config.nic.mac", MAC.ToString());
+        }
+        else
+        {
+            MAC = MacAddress.Parse(mac);
+        }
+
+        IP = GenerateRandomPublicIP();
+    }
+
+    public static IPAddress GenerateRandomPublicIP()
+    {
+        int firstOctet;
+
+        do
+        {
+            firstOctet = Random.Shared.Next(1, 224);
+        } while (firstOctet is >= 10 and <= 126); // exclude private IP-Ranges
+
+        var secondOctet = Random.Shared.Next(0, 256);
+        var thirdOctet = Random.Shared.Next(0, 256);
+        var fourthOctet = Random.Shared.Next(0, 256);
+
+        return IPAddress.Parse($"{firstOctet}.{secondOctet}.{thirdOctet}.{fourthOctet}");
+    }
+}
