61d300a1998ceb1f283c4a766a97da54d1a86be6
Chris <filmee24@gmail.com>
2024-10-31 12:05:17 +0100

    feat: Better MemoryAllocator with struct support

--- src/MimaSim/MimaSim/MIMA/Parsing/MemoryAllocator.cs
+++ src/MimaSim/MimaSim/MIMA/Parsing/MemoryAllocator.cs
@@ -2,22 +2,61 @@
 
 namespace MimaSim.MIMA.Parsing;
 
-public static class MemoryAllocator
+public class MemoryAllocator
 {
-    private static readonly Dictionary<string, byte> _variableAllocations = new();
+    private readonly Dictionary<string, (int Address, Type DataType)> _variables;
+    private int _nextAddress;
 
-    public static byte Allocate(string name)
+    public StaticMemoryAllocator()
     {
-        if (_variableAllocations.ContainsKey(name))
-        {
-            return _variableAllocations[name];
-        }
-        else
-        {
-            var address = (byte)(_variableAllocations.Count + 1);
-            _variableAllocations.Add(name, address);
+        _variables = new Dictionary<string, (int, Type)>();
+        _nextAddress = 0;
+    }
 
-            return address;
+    public void AllocateVariable(string name, Type dataType)
+    {
+        if (_variables.ContainsKey(name))
+        {
+            throw new ArgumentException($"Variable '{name}' ist bereits allokiert.");
         }
+
+        _variables[name] = (_nextAddress++, dataType);
+    }
+
+    public void AllocateStruct(string structName, Dictionary<string, Type> members)
+    {
+        if (_variables.ContainsKey(structName))
+        {
+            throw new ArgumentException($"Struct '{structName}' ist bereits allokiert.");
+        }
+
+        int structSize = CalculateStructSize(members);
+        _variables[structName] = (_nextAddress, typeof(StructWrapper));
+        _nextAddress += structSize;
+    }
+
+    private int CalculateStructSize(Dictionary<string, Type> members)
+    {
+        int size = 0;
+        foreach (var member in members.Values)
+        {
+            size += System.Runtime.InteropServices.Marshal.SizeOf(member);
+        }
+        return size;
+    }
+
+    public (int Address, Type DataType) GetVariableInfo(string name)
+    {
+        if (!_variables.TryGetValue(name, out var info))
+        {
+            throw new KeyNotFoundException($"Variable '{name}' wurde nicht gefunden.");
+        }
+        return info;
+    }
+
+    public class StructWrapper
+    {
+        public int Address { get; set; }
+        public Dictionary<string, Type> Members { get; set; }
     }
 }
